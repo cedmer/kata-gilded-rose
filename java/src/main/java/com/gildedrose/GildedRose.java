6f81aef79ecd402cec865e5470bf968fff94211c
cedmer <ced.mer@laposte.net>
2022-01-11 14:43:08 +0100

    add refactor Sulfuras

--- java/src/main/java/com/gildedrose/GildedRose.java
+++ java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,9 +17,7 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    handleSulfuras(i,"quality");
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -41,17 +39,13 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            handleSulfuras(i,"sellIn");
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                            handleSulfuras(i,"quality");
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
@@ -64,4 +58,16 @@ class GildedRose {
             }
         }
     }
+
+    private void handleSulfuras(int i,String itemOption) {
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if(itemOption.equals("quality")){
+                items[i].quality = items[i].quality - 1;
+            }
+            else {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+        }
+    }
 }
+
